61e77706f0d3387089127bf46176f47692a2e4b0
The Embedded Rustacean <166909466+theembeddedrustacean@users.noreply.github.com>
2025-02-09 11:17:31 +0300

    svc v0.51 Crate Update

--- led-bar-blink/src/main.rs
+++ led-bar-blink/src/main.rs
@@ -14,10 +14,10 @@ fn main() {
     let dp = Peripherals::take().unwrap();
 
     // Configure all LED pins to digital outputs
-    let mut led1 = PinDriver::output(dp.pins.gpio0).unwrap();
-    let mut led2 = PinDriver::output(dp.pins.gpio1).unwrap();
-    let mut led3 = PinDriver::output(dp.pins.gpio2).unwrap();
-    let mut led4 = PinDriver::output(dp.pins.gpio3).unwrap();
+    let mut led1 = PinDriver::output(dp.pins.gpio1).unwrap();
+    let mut led2 = PinDriver::output(dp.pins.gpio10).unwrap();
+    let mut led3 = PinDriver::output(dp.pins.gpio19).unwrap();
+    let mut led4 = PinDriver::output(dp.pins.gpio18).unwrap();
     let mut led5 = PinDriver::output(dp.pins.gpio4).unwrap();
     let mut led6 = PinDriver::output(dp.pins.gpio5).unwrap();
     let mut led7 = PinDriver::output(dp.pins.gpio6).unwrap();
@@ -26,7 +26,7 @@ fn main() {
     let mut led10 = PinDriver::output(dp.pins.gpio9).unwrap();
 
     // Configure Button pin to input with Pull Up
-    let mut button = PinDriver::input(dp.pins.gpio19).unwrap();
+    let mut button = PinDriver::input(dp.pins.gpio3).unwrap();
     button.set_pull(Pull::Up).unwrap();
 
     // Initialize variable with starting delay
@@ -115,7 +115,7 @@ fn main() {
     }
 }
 
-fn button_pressed(but: &PinDriver<'_, Gpio19, Input>, del: &u32) -> u32 {
+fn button_pressed(but: &PinDriver<'_, Gpio3, Input>, del: &u32) -> u32 {
     // Check if Button has been pressed
     // If not pressed, return the delay value unchanged
     if but.is_low() {
