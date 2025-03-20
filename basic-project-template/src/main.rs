a601cb96862ff5f973a37c65b8bb971705d53777
The Embedded Rustacean <166909466+theembeddedrustacean@users.noreply.github.com>
2025-03-20 09:35:10 +0300

    v1.2 Book Updates

---
+++ basic-project-template/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+}
