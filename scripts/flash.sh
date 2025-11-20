1c3f16fe770efe08d90f16f4983e285f6144280e
The Embedded Rustacean <166909466+theembeddedrustacean@users.noreply.github.com>
2025-11-20 07:48:20 +0300

    dependencies

---
+++ scripts/flash.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_MODE=""
+case "$1" in
+"" | "release")
+    bash scripts/build.sh
+    BUILD_MODE="release"
+    ;;
+"debug")
+    bash scripts/build.sh debug
+    BUILD_MODE="debug"
+    ;;
+*)
+    echo "Wrong argument. Only \"debug\"/\"release\" arguments are supported"
+    exit 1
+    ;;
+esac
+
+web-flash --chip esp32c3 target/riscv32imc-esp-espidf/${BUILD_MODE}/testidf
